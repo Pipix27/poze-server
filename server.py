28ca7ceaee4e5ad0edd48c1181e16cb42d50304e
Pipix27 <jianu.matei@gmail.com>
2025-06-28 12:06:11 +0300

    server.py

--- server.py
+++ server.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
 @app.route('/')
 def hello():
-    return "Server pornit!"
+    return 'Server pornit!'
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'image' not in request.files:
+        return 'No image part in request', 400
+
+    file = request.files['image']
+    if file.filename == '':
+        return 'No selected file', 400
+
+    filepath = os.path.join(UPLOAD_FOLDER, file.filename)
+    file.save(filepath)
+    return f'Poza {file.filename} a fost salvata cu succes!', 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    app.run()
